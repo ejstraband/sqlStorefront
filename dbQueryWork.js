cb8929dc9123d107eace4e2c460b41cb38b7bae9
Eric Traband <ejstraband@gmail.com>
2018-02-19 21:59:32 -0600

    sql responses parsed. Exit added

--- dbQueryWork.js
+++ dbQueryWork.js
@@ -4,8 +4,8 @@
 var inquirer = require('inquirer');
 var mysql = require('mysql');
 var console = require('better-console');
-var currentSelection;
-var currentQuantity;
+var itemSelection;
+var desiredQuantity;
 // =========================
 // sql setup
 var connection = mysql.createConnection({
@@ -18,14 +18,19 @@ var connection = mysql.createConnection({
     // Your password
     password: "yourpassword",
     database: "bamazon"
+
   });
 // =========================
 
 // testing
-itemRequested = process.argv[2];
-console.log("Item to try: " + itemRequested);
-currentQuantity = process.argv[3];
-console.log("Quantity Requested: " + currentQuantity);
+itemSelection = process.argv[2];
+console.log("Item to try: " + itemSelection);
+desiredQuantity = process.argv[3];
+console.log("Quantity Requested: " + desiredQuantity);
+
+connection.connect(function(err) {
+  if (err) throw err;
+});
 
 chooseFunction();
 
@@ -37,115 +42,75 @@ function chooseFunction() {
         type: "rawlist",
         message: "Pick a function",
         choices: [
-          "Display Inventory",
-        //   "Attempt a Purchase",
-          "Direct DB Quantity Check",
+        // display inventory table
+          "Display Store Inventory",
+        // "Attempt a Purchase",
+          "Attempt to Purchase",
+        // quantity query
+          "Item Quantity Check",
         // Calculate Total
-          "Calculate Purchase Total"
+          "Item Price Check",
+        // exit out
+          "Exit"
         ]
       })
       .then(function(answer) {
         switch (answer.action) {
-          case "Display Inventory":
+          case "Display Store Inventory":
             displayInventory();
             break;
-        //   case "Attempt a Purchase":
-        //     buySomething();
-        //     break;
-          case "Direct DB Quantity Check":
-            queryInventory();
+          case "Attempt to Purchase":
+            buySomething();
             break;
-          case "Calculate Purchase Total":
-            calculatePurchaseCost();
+          case "Item Quantity Check":
+            queryInventoryLevel();
             break;
+          case "Item Price Check":
+            lookUpItemCost();
+            break;
+          case "Exit":
+            process.exit(0);
         }
       });
-  }
+    }
 
 // Functions
 
-    // display inventory
-    function displayInventory() {
-        connection.connect(function(err) {
-            if (err) throw err;
-            // console.log("connected as id " + connection.threadId);
-            afterConnection();
-          });
-          
-          function afterConnection() {
-            connection.query("SELECT * FROM products", function(err, res) {
-              if (err) throw err;
-              console.table(res);
-              connection.end();
-              chooseFunction();
-            });
-          }
+// display inventory
+function displayInventory() {
+        // console.log("connected as id " + connection.threadId);
+        connection.query(("SELECT * FROM products"), function(err, res) {
+          if (err) throw err;
+          console.table(res);
+          chooseFunction();
+        });
+      }
+
+// display quantity
+function queryInventoryLevel() {
+      // console.log("connected as id " + connection.threadId);
+      connection.query(("SELECT stock_quantity FROM products WHERE item_id = " + itemSelection), function(err, res) {
+        if (err) throw err;
+        console.log("Let me check for you.\n");
+        var itemQuantityInStock = res[0].stock_quantity;
+        console.log(itemQuantityInStock + " of these in stock\n");
+        chooseFunction();
+      });
     }
 
-    // query inventory level
-    function queryInventory() {
-        connection.connect(function(err) {
-            if (err) throw err;
-            // console.log("connected as id " + connection.threadId);
-            getQuantityOnHand();
-            getPrice();
-            });
-            
-            function getQuantityOnHand() {
-            connection.query(("SELECT stock_quantity FROM products WHERE item_id = " + itemRequested), function(err, res) {
-                if (err) throw err;
-                console.log("Quantity Object" + res);
-                var resQuantJson = JSON.stringify(res);
-                console.log(resQuantJson);
-                // connection.end();
-            });
-            }
+// buy something
+function buySomething() {
+  console.log("buy something\n");
+  chooseFunction();
+}
 
-            function getPrice() {
-            connection.query(("SELECT price FROM products WHERE item_id = " + itemRequested), function(err, res) {
-                if (err) throw err;
-                console.log("Price Object" + res);
-                var resPriceJson = JSON.stringify(res);
-                console.log(resPriceJson);
-                connection.end();
-            });
-            }
-    }
-
-    // calculate purchase
-    function calculatePurchaseCost() {
-        var totalCost;
-        connection.connect(function(err) {
-            if (err) throw err;
-            // console.log("connected as id " + connection.threadId);
-            afterConnection();
-            });
-            
-            function afterConnection() {
-            connection.query(("SELECT price FROM products WHERE item_id = " + itemRequested), function(err, res) {
-                if (err) throw err;
-                console.log(res);
-                var totalPurchase = res * currentQuantity;
-                console.log(totalPurchase);
-                connection.end();
-            });
-            }
-    }
-
-
-
-    // stock check
-    // function checkInventory() {
-    //     connection.connect(function(err) {
-    //         if (err) throw err;
-    //         // console.log("connected as id " + connection.threadId);
-    //         afterConnection();
-    //       });
-          
-    //       function afterConnection() {
-    //         connection.query("SELECT stock_quantity FROM products WHERE item_id IS " + currentSelection, function(err, res) {
-    //           if (err) throw err;
-    //           console.table(res);
-    //           connection.end();
-    //         });
-    //       }
+// look up price
+function lookUpItemCost() {
+  console.log("Price Check!\n");
+  connection.query(("SELECT price FROM products WHERE item_id = " + itemSelection), function(err, res) {
+    if (err) throw err;
+    var itemPrice = res[0].price;
+    console.log("$" + itemPrice + ".00 Each\n");
+  chooseFunction();
+});
+}
